6928af79e4d016d5aa70ee66f34ae0319f0f601d
Amanpreet Singh <amanpreetsingh505@gmail.com>
2020-04-20 11:30:34 +0530

    Added style sheet

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  NativeModules,Alert
+  NativeModules,StyleSheet
 } from 'react-native';
 
 export default class App extends React.Component {
@@ -34,36 +34,62 @@ export default class App extends React.Component {
   }
 
   savePasswordBtnClicked = () => {
-    NativeModules.KeyChainWrapper.savePassword(this.state.password, value=>{
-      debugger
-      if (value){
-        alert("Password saved successfully")
-      }
-      else{
-        alert("Some error occured while saving password")
-      }
-    })
+    if (this.state.password == ""){
+      alert("Please enter valid password")
+      return
+    }
+    else {
+        NativeModules.KeyChainWrapper.savePassword(this.state.password, value=>{
+          debugger
+          if (value){
+            alert("Password saved successfully")
+          }
+          else{
+            alert("Some error occured while saving password")
+          }
+        })
+    }
   }
 
   render(){
   return(
-    <View style={{flex:1, flexDirection:"column",
-    justifyContent:"center",alignItems:"center"}}>
+    <View style={styles.container}>
       <TextInput style= {{width:300,height:40, fontWeight: "bold", fontSize:15,
       backgroundColor:"lightgrey",paddingLeft:20}} 
       placeholder="Enter password"
       onChangeText={this.handleChangePassword}
         value ={this.state.token}>
       </TextInput>
-      <TouchableOpacity style={{height:25,
-      backgroundColor:"green",marginTop:20,
-      height:40,width:120,justifyContent:"center",
-      alignItems:"center"}}
+      <TouchableOpacity style={styles.touchableButton}
       onPress = {this.savePasswordBtnClicked}
       >
-               <Text style={{fontSize:15,fontWeight: "bold", color:"grey",color:"white"}}>Save Password</Text>
+         <Text style={styles.button}>Save Password</Text>
         </TouchableOpacity>
     </View>
   );
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  button: {
+    fontSize:15,
+    fontWeight: "bold", 
+    color:"grey",
+    color:"white"
+
+  },
+  touchableButton:{
+    height:25,
+      backgroundColor:"green",
+      marginTop:20,
+      height:40,
+      width:120,
+      justifyContent:"center",
+      alignItems:"center"}
+});
